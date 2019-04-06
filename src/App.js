95352bc15b76d7257a3cffecb9771dd8d50c1911
Oleksandr Khomenko <homes89@ukr.net>
2019-04-06 20:11:30 +0300

    Codeformat

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { client } from "./services/api";
+import gql from "graphql-tag";
 
 class App extends Component {
+  componentDidMount() {
+    client
+      .query({
+        query: gql`
+          {
+            hops {
+              name
+              origin {
+                name
+              }
+            }
+          }
+        `
+      })
+      .then(result => console.log("got hops", result));
+  }
+
   render() {
     return (
       <div className="App">
