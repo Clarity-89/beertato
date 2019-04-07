31d21010950dd686923558e49def8e6fd283797e
Oleksandr Khomenko <homes89@ukr.net>
2019-04-07 17:43:52 +0300

    Add generator

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ *
+ * Navbar
+ *
+ */
+const Navbar = props => {
+  return <div>Navbar</div>;
+};
+
+Navbar.propTypes = {};
+
+export default Navbar;
