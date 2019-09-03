8cc70f7f9b357804742b423910a17e6f6ae77666
Oleksandr Khomenko <homes89@ukr.net>
2019-09-03 20:01:26 +0300

    Setup Search

---
+++ src/components/Search/Search.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+// import styled from "styled-components";
+
+/**
+ *
+ * Search
+ *
+ */
+const Search = (props) => {
+  return (
+    <div>
+      Search
+    </div>
+  );
+}
+
+Search.propTypes = {};
+
+export default Search;
