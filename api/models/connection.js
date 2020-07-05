3d4c7e418adaf5702f32fcd1f89fe6866a346c0d
Clarity-89 <homes89@ukr.net>
2020-07-05 10:23:21 +0300

    Remove models

--- api/models/connection.js
+++
@@ -1,6 +0,0 @@
-const Knex = require("knex");
-const connection = require("../knexfile");
-const { Model } = require("objection");
-const knexConnection = Knex(connection);
-
-module.exports = Model.knex(knexConnection);
--- api/models/hops.js
+++
@@ -1,7 +0,0 @@
-const Model = require("./connection");
-
-class Hop extends Model {
-  static tableName = "hops";
-}
-
-module.exports = Hop;
